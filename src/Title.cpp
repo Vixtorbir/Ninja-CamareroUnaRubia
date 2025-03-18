c57b0ccc3c135e1c504627cd7fc798d8d7af205e
Pau González Cayuela <gpaucayuela@gmail.com>
2025-03-18 10:29:38 +0100

    a

--- src/Title.cpp
+++ src/Title.cpp
@@ -32,10 +32,6 @@ bool Title::Awake()
 	LOG("Loading Title");
 	bool ret = true;
 
-	SDL_Rect btPos = { 520, 350, 120,20 };
-	guiBt = (GuiControlButton*) Engine::GetInstance().guiManager->CreateGuiControl(GuiControlType::BUTTON, 1, "ni", btPos, this);
-	
-	
 	return ret;
 }
 
