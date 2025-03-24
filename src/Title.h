e92be28b9a6c83a72e64a4dfacaf2197d232a001
noapont <17pontnoa@gmail.com>
2025-03-24 10:48:00 +0100

    ay nose

--- src/Title.h
+++ src/Title.h
@@ -15,62 +15,62 @@ class Title : public Module
 {
 public:
 
-	Title();
+    Title();
 
-	// Destructor
-	virtual ~Title();
+    // Destructor
+    virtual ~Title();
 
-	// Called before render is available
-	bool Awake();
+    // Called before render is available
+    bool Awake();
 
-	// Called before the first frame
-	bool Start();
+    // Called before the first frame
+    bool Start();
 
-	// Called before all Updates
-	bool PreUpdate();
+    // Called before all Updates
+    bool PreUpdate();
 
-	// Called each loop iteration
-	bool Update(float dt);
+    // Called each loop iteration
+    bool Update(float dt);
 
-	// Called before all Updates
-	bool PostUpdate();
+    // Called before all Updates
+    bool PostUpdate();
 
-	// Called before quitting
-	bool CleanUp();
+    // Called before quitting
+    bool CleanUp();
 
-	// Return the player position
-	Vector2D GetPlayerPosition();
+    // Return the player position
+    Vector2D GetPlayerPosition();
 
-	//L15 TODO 1: Implement the Load function
-	void LoadState();
-	//L15 TODO 2: Implement the Save function
-	void SaveState();
+    //L15 TODO 1: Implement the Load function
+    void LoadState();
+    //L15 TODO 2: Implement the Save function
+    void SaveState();
 
-	// Handles multiple Gui Event methods
-	bool OnGuiMouseClickEvent(GuiControl* control);
+    // Handles multiple Gui Event methods
+    bool OnGuiMouseClickEvent(GuiControl* control);
 
-	void LoadTextures();
-
-public:
-	// Get tilePosDebug value
+    void LoadTextures();
 
 public:
+    // Get tilePosDebug value
 
+public:
 
-	bool once = false;
+    bool once = false;
 
-	//L03: TODO 3b: Declare a Player attribute
-	Player* player;
-	std::vector<Enemy*> enemyList;
+    //L03: TODO 3b: Declare a Player attribute
+    Player* player;
+    std::vector<Enemy*> enemyList;
 
+    // L16: TODO 2: Declare a GUI Control Button 
+    GuiControlButton* guiBt;
 
-	// L16: TODO 2: Declare a GUI Control Button 
-	GuiControlButton* guiBt;
+    Dialogue* dialogue;
+    DialogueManager* dialogueManager;
 
-	Dialogue* dialogue;
-	DialogueManager* dialogueManager;
+    // Add a boolean to track if the button should be displayed
+    bool showStartButton;
 
 private:
 
 };
-
