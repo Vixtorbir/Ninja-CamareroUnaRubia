9cc94df0c2f9350ed3f80c7bf1c8f7ad4eb488e9
Pau González Cayuela <gpaucayuela@gmail.com>
2025-05-07 09:21:13 +0200

    Revert "Merge remote-tracking branch 'origin/PaulaAnimations'"
    This reverts commit 69c00ce60a28058ae92132a1391137863269b9c1, reversing
    changes made to 15cbe3dc987d283c9649c7869c640fe0d61f27c0.

--- Assets/Maps/level2CaveTileset 128 x 128.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="level2CaveTileset 128 x 128" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
- <image source="../Backgrounds/level2CaveTileset 128 x 128.png" width="2048" height="2048"/>
-</tileset>
