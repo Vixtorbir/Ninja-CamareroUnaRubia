4c2e6d901e8eeb6de1ea8c019f50f9c0b39bed9f
kiwiipow <paula2005nemo@gmail.com>
2025-05-07 09:47:07 +0200

    map2 fix

---
+++ Assets/Maps/level2CaveTileset 128 x 128 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="level2CaveTileset 128 x 128" tilewidth="128" tileheight="128" tilecount="256" columns="16">
+ <image source="../Backgrounds/level2CaveTileset 128 x 128.png" width="2048" height="2048"/>
+</tileset>
